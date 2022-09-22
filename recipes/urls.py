11299ef5767b74c36732a047012b17044edf3dd6
Palmieres Mota <palmierres22@gmail.com>
2022-09-22 16:31:57 -0300

    update

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from recipes.views import home, contato, sobre
+from recipes.views import home 
 
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
 
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
