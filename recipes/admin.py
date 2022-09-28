9ae6eb866066e6d248370e78a442efc660fd6922
Palmieres Mota <palmierres22@gmail.com>
2022-09-28 18:02:14 -0300

    atualizacao do models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models  import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
