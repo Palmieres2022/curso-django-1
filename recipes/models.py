37909988402a2881e47ad9c793f814819d16614a
Palmieres Mota <palmierres22@gmail.com>
2022-09-28 17:09:07 -0300

    update

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,37 @@
+from distutils.command.upload import upload
+from email.policy import default
+from turtle import title, update
+from unicodedata import category
+from unittest.util import _MAX_LENGTH
+
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+    
+    
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 
+app_name = 'recipes'
+
+
 urlpatterns = [
-    path('', views.home, name="recipes-home"),
-    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
