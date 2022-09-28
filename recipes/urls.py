4688c2773ce3bc59ffa7cae11cecf8ba1b2736ee
Palmieres Mota <palmierres22@gmail.com>
2022-09-28 17:09:01 -0300

    update

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -4,12 +4,13 @@ from utils.recipes.factory import make_recipe
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={ 
-        'recipes': [make_recipe for _ in range(10)],
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_datail_page': True,
     })
 
