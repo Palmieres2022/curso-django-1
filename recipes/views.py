6beda9a055851fab364696577b208b6a19913889
Palmieres Mota <palmierres22@gmail.com>
2022-09-30 15:24:09 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
-
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            is_published=True,
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -26,7 +29,9 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
